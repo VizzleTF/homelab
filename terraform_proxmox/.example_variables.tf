94724665a6d839490a8d25b9d5eb6468684ada43
VizzleTF <vizzlef@gmail.com>
2024-03-23 20:36:34 +0300

    small changes

--- terraform_proxmox/.example_variables.tf
+++ terraform_proxmox/.example_variables.tf
@@ -1,7 +1,19 @@
 variable "home_pc_public_key" {
-  default = "ssh-rsa AAA*** user@WIN-BKRCDUJNE4Q"
+  default = "ssh-ed25519 AAAA"
 }
 
 variable "endpoint" {
-  default = "https://10.11.12.13:8006/"
+  default = "https://10.11.12.19:8006/"
+}
+
+variable "vm_password" {
+  default = ""
+}
+
+variable "proxmox_username" {
+  default = "root@pam"
+}
+
+variable "main_password" {
+  default = ""
 }
