5bd27d6eac52d3b95e05b979cd780c6ffbbfebf7
VizzleTF <vizzlef@gmail.com>
2024-07-23 11:28:24 +0300

    update images.tf, provider.tf, vm_locals.tf and vm_resources.tf

--- terraform_proxmox/images.tf
+++ terraform_proxmox/images.tf
@@ -8,45 +8,53 @@
 #   }
 # }
 
-resource "proxmox_virtual_environment_download_file" "pve2_fedora_coreos_image" {
-  content_type = "iso"
-  datastore_id = "local"
-  node_name    = "pve2"
-  url          = "https://builds.coreos.fedoraproject.org/prod/streams/stable/builds/39.20240128.3.0/x86_64/fedora-coreos-39.20240128.3.0-live.x86_64.iso"
-  file_name    = "fedora_core_os39.img"
-}
+# resource "proxmox_virtual_environment_download_file" "pve2_fedora_coreos_image" {
+#   content_type = "iso"
+#   datastore_id = "local"
+#   node_name    = "pve2"
+#   url          = "https://builds.coreos.fedoraproject.org/prod/streams/stable/builds/39.20240128.3.0/x86_64/fedora-coreos-39.20240128.3.0-live.x86_64.iso"
+#   file_name    = "fedora_core_os39.img"
+# }
 
-resource "proxmox_virtual_environment_download_file" "home_fedora_coreos_image" {
-  content_type = "iso"
-  datastore_id = "local"
-  node_name    = "home"
-  url          = "https://builds.coreos.fedoraproject.org/prod/streams/stable/builds/39.20240128.3.0/x86_64/fedora-coreos-39.20240128.3.0-live.x86_64.iso"
-  file_name    = "fedora_core_os39.img"
-}
+# resource "proxmox_virtual_environment_download_file" "home_fedora_coreos_image" {
+#   content_type = "iso"
+#   datastore_id = "local"
+#   node_name    = "home"
+#   url          = "https://builds.coreos.fedoraproject.org/prod/streams/stable/builds/39.20240128.3.0/x86_64/fedora-coreos-39.20240128.3.0-live.x86_64.iso"
+#   file_name    = "fedora_core_os39.img"
+# }
 
-resource "proxmox_virtual_environment_download_file" "fedora_cloud_image" {
-  content_type = "iso"
-  datastore_id = "local"
-  node_name    = "pve2"
-  url          = "https://download.fedoraproject.org/pub/fedora/linux/releases/39/Cloud/x86_64/images/Fedora-Cloud-Base-39-1.5.x86_64.qcow2"
-  file_name    = "fedora39.img"
+# resource "proxmox_virtual_environment_download_file" "fedora_cloud_image" {
+#   content_type = "iso"
+#   datastore_id = "local"
+#   node_name    = "pve2"
+#   url          = "https://download.fedoraproject.org/pub/fedora/linux/releases/39/Cloud/x86_64/images/Fedora-Cloud-Base-39-1.5.x86_64.qcow2"
+#   file_name    = "fedora39.img"
+# }
+locals {
+  nodes = [
+    { node_name = "home" },
+    { node_name = "pve3" },
+    { node_name = "pve4" },
+    { node_name = "pve5" },
+  ]
 }
-
 
 resource "proxmox_virtual_environment_download_file" "oracle_cloud_image" {
+  for_each = { for x in local.nodes : x.node_name => x }
   content_type = "iso"
   datastore_id = "local"
-  node_name    = "pve2"
-  url          = "https://yum.oracle.com/templates/OracleLinux/OL9/u3/x86_64/OL9U3_x86_64-kvm-b211.qcow"
-  file_name    = "oracle93.img"
+  node_name    = each.value.node_name
+  url          = "https://yum.oracle.com/templates/OracleLinux/OL9/u4/x86_64/OL9U4_x86_64-kvm-b234.qcow2"
+  file_name    = "oracle94.img"
 }
 
-resource "proxmox_virtual_environment_file" "pbs_image" {
-  content_type = "iso"
-  datastore_id = "local"
-  node_name    = "home"
+# resource "proxmox_virtual_environment_file" "pbs_image" {
+#   content_type = "iso"
+#   datastore_id = "local"
+#   node_name    = "home"
 
-  source_file {
-    path = "https://enterprise.proxmox.com/iso/proxmox-backup-server_3.1-1.iso"
-  }
-}
+#   source_file {
+#     path = "https://enterprise.proxmox.com/iso/proxmox-backup-server_3.1-1.iso"
+#   }
+# }
--- terraform_proxmox/vm_resources.tf
+++ terraform_proxmox/vm_resources.tf
@@ -1,140 +1,43 @@
 ### Proxmox backup server ###
+#   disk {
+#     datastore_id = "data"
+#     file_id      = proxmox_virtual_environment_file.pbs_image.id
+#     interface    = "scsi0"
+#   }
 
-resource "proxmox_virtual_environment_vm" "pbs" {
-  name      = "proxmox-backup-server"
-  tags      = ["terraform", "pbs"]
-  node_name = "home"
-  vm_id     = 101
-
-  cpu {
-    cores = "2"
-  }
-  memory {
-    dedicated = "2048"
-  }
-  startup {
-    order    = "2"
-    up_delay = "5"
-  }
-  disk {
-    datastore_id = "data"
-    file_id      = proxmox_virtual_environment_file.pbs_image.id
-    interface    = "scsi0"
-  }
-  disk {
-    datastore_id = "data"
-    file_format  = "raw"
-    interface    = "scsi1"
-    size         = 15
-  }
-  disk {
-    datastore_id = "data"
-    file_format  = "raw"
-    interface    = "scsi2"
-    size         = 150
-  }
-  initialization {
-    ip_config {
-      ipv4 {
-        address = "10.11.12.211/24"
-        gateway = "10.11.12.1"
-      }
-    }
-    user_account {
-      keys     = [trimspace(var.home_pc_public_key)]
-      password = var.vm_password
-      username = "root"
-    }
-  }
-  network_device {
-    bridge = "vmbr0"
-  }
-  operating_system {
-    type = "l26"
-  }
+locals {
+  vms_config = yamldecode(file("./configs/vms.yaml"))
+  # os = {
+  #   oracle_cloud_image = proxmox_virtual_environment_download_file.oracle_cloud_image[0]
+  # }
 }
 
-### OKD NODES ###
+resource "proxmox_virtual_environment_vm" "vm" {
+  # for_each   = { for vm in local.oracle_linux : vm.name => vm }
+  for_each = { for x in local.vms_config.vms : x.vm_name => x }
 
-resource "proxmox_virtual_environment_vm" "fedora_coreos_image" {
-  for_each   = { for vm in local.okd_nodes : vm.name => vm }
-  name       = each.value.name
-  tags       = ["terraform", "coreos", "okd"]
-  node_name  = each.value.node_name
-  vm_id      = each.value.vm_id
-  boot_order = ["scsi0"] # after ignite
-  # boot_order = ["scsi1"] # before ignite
-  # started    = false
-
-  cpu {
-    cores = each.value.cores
-    type  = "host"
-  }
-  memory {
-    dedicated = each.value.memory
-  }
-  startup {
-    order    = "3"
-    up_delay = "10"
-  }
-  disk {
-    datastore_id = each.value.datastore_id
-    file_format  = "raw"
-    interface    = "scsi0"
-    size         = 50
-  }
-  disk {
-    datastore_id = each.value.datastore_id
-    file_id      = "local:iso/fedora_core_os39.img"
-    interface    = "scsi1"
-  }
-  initialization {
-    ip_config {
-      ipv4 {
-        address = each.value.address
-        gateway = "10.11.12.1"
-      }
-    }
-    user_account {
-      keys     = [trimspace(var.home_pc_public_key)]
-      password = var.vm_password
-      username = "root"
-    }
-  }
-  network_device {
-    bridge      = "vmbr0"
-    mac_address = each.value.mac_address
-  }
-  operating_system {
-    type = "l26"
-  }
-}
-
-resource "proxmox_virtual_environment_vm" "oracle_cloud_linux" {
-  for_each   = { for vm in local.oracle_linux : vm.name => vm }
-  name       = each.value.name
-  tags       = each.value.tags
-  node_name  = "pve2"
+  name       = each.value.vm_name
+  tags       = concat(local.vms_config.tags,each.value.tags)
+  node_name  = try(each.value.node_name, "pve5")
   vm_id      = each.value.vm_id
   boot_order = ["sata0"]
 
   cpu {
-    cores        = "2"
-    type         = "Haswell"
+    cores        = try(each.value.cores, "2")
+    type         = "host"
     architecture = "x86_64"
   }
-  memory {
-    dedicated = "2048"
-  }
+  memory { dedicated = try(each.value.ram, "2048") }
   startup {
     order    = "2"
     up_delay = "5"
   }
   disk {
     datastore_id = "local-lvm"
-    file_id      = proxmox_virtual_environment_download_file.oracle_cloud_image.id
+    file_id      = proxmox_virtual_environment_download_file.oracle_cloud_image[each.value.node_name].id
+    # file_id      = try(local.os[each.value.os], proxmox_virtual_environment_download_file.oracle_cloud_image[each.value.node_name])
     interface    = "sata0"
-    size         = 50
+    size         = try(each.value.disk_size, 50)
   }
   initialization {
     ip_config {
@@ -149,10 +52,10 @@ resource "proxmox_virtual_environment_vm" "oracle_cloud_linux" {
       username = "root"
     }
   }
-  network_device {
-    bridge = "vmbr0"
-  }
-  operating_system {
-    type = "l26"
-  }
+  network_device { bridge = "vmbr0" }
+  operating_system { type = "l26" }
 }
+
+# output "images" {
+#   value = proxmox_virtual_environment_download_file.oracle_cloud_image
+# }
