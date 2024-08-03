a6f86abd9111694c31017f98067112e8732982bc
VizzleTF <vizzlef@gmail.com>
2024-08-03 16:24:08 +0300

    change module version in images.tf and vm_resources.tf

--- terraform_proxmox/images.tf
+++ terraform_proxmox/images.tf
@@ -5,7 +5,7 @@ locals {
 
 module "images" {
   for_each = { for image in local.images_config.images : image.image_name => image }
-  source   = "./modules/images"
+  source   = "git@github.com:VizzleTF/home_proxmox.git//terraform_proxmox/modules/images?ref=v1.0.1"
 
   url       = each.value.image_url
   file_name = "${each.value.image_name}.img"
--- terraform_proxmox/vm_resources.tf
+++ terraform_proxmox/vm_resources.tf
@@ -4,7 +4,7 @@ locals {
 
 module "vms" {
   for_each = { for vm in local.vms_config.vms : vm.vm_name => vm }
-  source   = "git@github.com:VizzleTF/home_proxmox.git//terraform_proxmox/modules/vms?ref=v1.0.0"
+  source   = "git@github.com:VizzleTF/home_proxmox.git//terraform_proxmox/modules/vms?ref=v1.0.1"
 
   vm_name            = each.value.vm_name
   node_name          = try(each.value.node_name, "pve5")
