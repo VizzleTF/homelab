904bdb9375b4fff93902c8e0881147d2dbc33def
VizzleTF <vizzlef@gmail.com>
2024-08-03 16:22:33 +0300

    refactor

--- terraform_proxmox/modules/vms/variables.tf
+++ terraform_proxmox/modules/vms/variables.tf
@@ -1,11 +1,66 @@
-variable "vm_name" { default = null }
-variable "node_name" { default = null }
-variable "tags" { default = null }
-variable "vm_id" { default = null }
-variable "cores" { default = null }
-variable "ram" { default = null }
-variable "disk_size" { default = null }
-variable "address" { default = null }
-variable "home_pc_public_key" { default = null }
-variable "vm_password" { default = null }
-variable "image_file" { default = null }
+variable "vm_name" {
+  description = "Name of the VM"
+  type        = string
+  default     = null
+}
+
+variable "node_name" {
+  description = "Name of the node where the VM will be created"
+  type        = string
+  default     = null
+}
+
+variable "tags" {
+  description = "List of tags to be associated with the VM"
+  type        = list(string)
+  default     = null
+}
+
+variable "vm_id" {
+  description = "ID of the VM"
+  type        = number
+  default     = null
+}
+
+variable "cores" {
+  description = "Number of CPU cores for the VM"
+  type        = number
+  default     = null
+}
+
+variable "ram" {
+  description = "Amount of RAM for the VM"
+  type        = number
+  default     = null
+}
+
+variable "disk_size" {
+  description = "Size of the disk for the VM"
+  type        = number
+  default     = null
+}
+
+variable "address" {
+  description = "IP address for the VM"
+  type        = string
+  default     = null
+}
+
+variable "home_pc_public_key" {
+  description = "Public key for SSH access"
+  type        = string
+  default     = null
+}
+
+variable "vm_password" {
+  description = "Password for the VM"
+  type        = string
+  default     = null
+}
+
+variable "image_file" {
+  description = "Path to the image file"
+  type        = string
+  default     = null
+}
+
