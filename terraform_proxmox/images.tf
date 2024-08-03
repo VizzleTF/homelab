fb8cf0ab318ec8f79fb4024e0402b2912239b82e
VizzleTF <vizzlef@gmail.com>
2024-08-03 15:55:04 +0300

    formated

--- terraform_proxmox/images.tf
+++ terraform_proxmox/images.tf
@@ -1,14 +1,14 @@
 
 locals {
   images_config = yamldecode(file("./configs/images.yaml"))
-  nodes = toset(["home", "pve3", "pve4", "pve5"])
+  nodes         = toset(["home", "pve3", "pve4", "pve5"])
 }
 
 module "images" {
   for_each = { for image in local.images_config.images : image.image_name => image }
-  source = "git@github.com:VizzleTF/home_proxmox.git//terraform_proxmox/modules/images?ref=v1.0.0"
+  source   = "git@github.com:VizzleTF/home_proxmox.git//terraform_proxmox/modules/images?ref=v1.0.0"
 
-  nodes = local.nodes
-  url          = each.value.image_url
-  file_name    = "${each.value.image_name}.img"
+  nodes     = local.nodes
+  url       = each.value.image_url
+  file_name = "${each.value.image_name}.img"
 }
