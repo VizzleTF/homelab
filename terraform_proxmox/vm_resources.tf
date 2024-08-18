06af2e66d4e178b34788ffe3d6627702ee9e5871
VizzleTF <vizzlef@gmail.com>
2024-08-18 19:19:38 +0300

    add host usb feature

--- terraform_proxmox/vm_resources.tf
+++ terraform_proxmox/vm_resources.tf
@@ -1,6 +1,5 @@
 locals {
-  vms_config     = yamldecode(file("./configs/vms.yaml"))
-  required_nodes = ["kube-node-01", "kube-node-02", "kube-node-03"]
+  vms_config = yamldecode(file("./configs/vms.yaml"))
 }
 module "vms" {
   for_each = { for vm in local.vms_config.vms : vm.vm_name => vm }
@@ -18,18 +17,5 @@ module "vms" {
   home_pc_public_key = file("~/.ssh/id_rsa.pub")
   image_file         = try(module.images[each.value.image_name].images[each.value.node_name].id, module.images["ol94"].images[each.value.node_name].id, module.images["ol94"].images["pve5"].id)
   pool_id            = try(each.value.pool_id, null)
-}
-
-resource "null_resource" "run_k8s_script" {
-  count = length(setintersection(keys(module.vms), local.required_nodes)) == 3 ? 1 : 0
-
-  depends_on = [module.vms, local_file.ansible_inventory]
-
-  provisioner "local-exec" {
-    command = "./cluster_create.sh"
-  }
-
-  triggers = {
-    vms_created = join(",", [for name in local.required_nodes : module.vms[name].vm_id])
-  }
+  usb                = try(each.value.usb, null)
 }
