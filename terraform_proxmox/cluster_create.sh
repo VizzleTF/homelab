2d5dcc2e6b693dfb490d16379dc9cf201bfb3465
VizzleTF <vizzlef@gmail.com>
2024-08-24 04:47:42 +0300

    Add Ollama charts repository and Ollama deployment Update kubespray playbook inventory path

--- terraform_proxmox/cluster_create.sh
+++ terraform_proxmox/cluster_create.sh
@@ -5,8 +5,8 @@ ssh-keyscan 10.11.12.242 >> ~/.ssh/known_hosts
 ssh-keyscan 10.11.12.243 >> ~/.ssh/known_hosts
 cd ../ansible
 ansible-playbook playbooks/oracle_new_vm.yaml -i inventory/inventory.yaml
-cd ../kubespray
-ansible-playbook -i ../ansible/inventory/inventory.yaml  --become --become-user=root cluster.yml
+cd ../../kubespray
+ansible-playbook -i ../home_proxmox/ansible/inventory/inventory.yaml  --become --become-user=root cluster.yml
 scp root@10.11.12.241:~/.kube/config ~/.kube/config
 sed -i'' -e "s^server:.*^server: https://10.11.12.241:6443^" ~/.kube/config
 kubectl get nodes -o wide
