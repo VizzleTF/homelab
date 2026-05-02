e1fec5c823e09d14ba5c1e1dab3ae73b7949f4e6
VizzleTF <vizzlef@gmail.com>
2026-05-02 09:10:18 +0300

    chore(deps): Update Terraform talos to v0.11.0 (#11)
    Co-authored-by: Renovate Bot <renovate@vaka.work>

--- terraform_proxmox/provider.tf
+++ terraform_proxmox/provider.tf
@@ -11,7 +11,7 @@ terraform {
     }
     talos = {
       source  = "siderolabs/talos"
-      version = "0.11.0-beta.2"
+      version = "0.11.0"
     }
     time = {
       source  = "hashicorp/time"
